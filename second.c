48dc5a843c6a34787b4e602d1e65a9624d36926b
ShikhaChaudhary <70765994+nirwalsc@users.noreply.github.com>
2021-01-14 23:22:39 +0530

    Add files via upload

---
+++ second.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter a number a=");
+    scanf("%d",&a);
+    printf("enter a number b=");
+    scanf("%d",&b);
+    printf("sum=%d",a+b);
+
+    return 0;
+    
+}
