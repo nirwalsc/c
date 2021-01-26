5c9174892e201f61d419371e005439bec3256c53
ShikhaChaudhary <70765994+nirwalsc@users.noreply.github.com>
2021-01-26 23:09:14 +0530

    Add files via upload

---
+++ hcf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,h;
+    printf("enter two numbers ");
+    scanf("%d%d",&a,&b);
+    for(h=a<b?a:b; h>=1;h--)
+    if(a%h==0&&b%h==0)
+    break;
+    printf(" hcf is %d",h);
+    return 0;
+}
