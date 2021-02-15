84d2e9e9bf70d36637fb55beeaecf66b00e5f59d
ShikhaChaudhary <70765994+nirwalsc@users.noreply.github.com>
2021-02-15 20:29:28 +0530

    Add files via upload

---
+++ funcsum.c
@@ -0,0 +1,20 @@
+#include<stdio.h>  
+int sum();  
+void main()  
+{  
+    int result;   
+    printf("\n the sum of two numbers:");  
+    result = sum();  
+    printf("%d",result);  
+}  
+int sum()  
+{  
+    int a,b;   
+    printf("\nEnter two numbers");  
+    scanf("%d %d",&a,&b);  
+    return a+b;   
+}  
+
+
+
+
