fe2120bf5c717abac5fd4997962a29e305dcfa5b
ShikhaChaudhary <70765994+nirwalsc@users.noreply.github.com>
2021-02-22 21:55:08 +0530

    Add files via upload

---
+++ aoet.c
@@ -0,0 +1,10 @@
+#include<stdio.h>  
+ int main()   
+  {
+   double a, area; 
+   printf("enter value=");
+   scanf("%lf",&a) ;
+   area = ( 1.73 * a*a) / 4 ;  
+  printf("\n Area of Equilateral Triangle is : %lf",area);  
+  return (0);  
+    }  
