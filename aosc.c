d3458fa9e301cad6c81b46e4c676fd6465bfa94c
ShikhaChaudhary <70765994+nirwalsc@users.noreply.github.com>
2021-02-27 21:29:12 +0530

    Add files via upload

---
+++ aosc.c
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+int main()
+{
+	int r;
+	float area;
+	printf("enter radius of the circle: ");
+	scanf("%d",&r);
+	area=(22*r*r)/(2*7);
+	printf("AOSC: %f\n",area);
+	return 0;
+}
