fd878e9f2d319829a78d1241c09a0916bebc5c46
ShikhaChaudhary <70765994+nirwalsc@users.noreply.github.com>
2021-07-15 16:36:22 +0530

    Add files via upload

---
+++ aopolygon.c
@@ -0,0 +1,8 @@
+#include <stdio.h>  
+int main()  
+{  
+   int s,a;  
+   printf("enter value:");
+   double area_pentagon=(5.0/2.0)*s*a;  
+   printf("Area of the pentagon=%lf",area_pentagon);  
+}  
